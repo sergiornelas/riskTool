05701a8ea08e907558f84e5e627c964b1f5ffbab
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-03 01:28:57 -0600

    se acomodo el footer, se creo la pagina de excluir parche

--- clients/urls.py
+++ clients/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
    	path('', views.index, name='patchlizt'),
+    path('exclude', views.exclude, name='ezclude'),
 
     #first parameter: empty path means home directory like /
     #second parameter: method we want to connect in the view file
