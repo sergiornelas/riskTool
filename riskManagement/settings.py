d3feeccad21c77f4ce0711ec2d12846c0410e6b2
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-08 18:26:06 -0600

    conexion con base de datos completa}

--- riskManagement/settings.py
+++ riskManagement/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'riskManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'riskManagement',
+        # 'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
