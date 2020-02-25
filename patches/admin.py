42e236c94483f422d7e1c7ee6eff847c6404269b
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-24 20:18:13 -0600

    anadidos campos del admin

--- patches/admin.py
+++ patches/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import patch
 from .models import exclude_patch
 
-admin.site.register(patch)
-admin.site.register(exclude_patch)
+class PatchesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'server_package', 'time', 'criticality')
+    list_display_links = ('id', 'server_package')
+    search_fields = ('server_package',)
+    list_per_page = 25
+
+class ExcludePatchesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'justification', 'excludeDate')
+    list_display_links = ('id', 'title')
+    search_fields = ('excludeDate',)
+    list_per_page = 25
+
+admin.site.register(patch, PatchesAdmin)
+admin.site.register(exclude_patch, ExcludePatchesAdmin)
