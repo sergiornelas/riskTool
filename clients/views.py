c1ed0093063d32d53b7ea73333176be3444772e5
sergiornelas <sergio.ornelas.92@outlook.com>
2020-02-25 23:07:57 -0600

    datos a frontend parte 1

--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#show data frontend
+#BOOOM!!, agarré el modelo de la aplicación de patches.
+from patches.models import patch
+
 # Create your views here.
 def index(request):
-	return render(request, 'clients/patchList.html')
+
+	patches = patch.objects.all() #agarramos todos los objetos (elementos de bases de datos)
+				     #los almacenamos en la variable listings
+	context = {
+    	'patches': patches #almacenamos los valores de la bd a un diccionario.
+    }
+
+	return render(request, 'patches/patchList.html', context)
 
 def exclude(request):
-	return render(request, 'clients/excludePatch.html')
+	return render(request, 'patches/excludePatch.html')
