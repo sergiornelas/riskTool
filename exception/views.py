70c667df306de178cc139684342d774b0a2e14cc
sergiornelas <sergio.ornelas.92@outlook.com>
2020-05-19 19:03:05 -0500

    ya no se puede enviar excepciones del mismo parche

--- exception/views.py
+++ exception/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.contrib import messages
-
-#from django.core.mail import send_mail
-
 from .models import exclude_patch
-from patches.models import patch
-
-from django.contrib.auth.models import User
+#from django.core.mail import send_mail
 
 def exclude(request):
     if request.method == 'POST':
@@ -16,14 +11,13 @@ def exclude(request):
         title = request.POST['title']
         justification = request.POST['justification']
         exclude_date = request.POST['exclude_date']
-        
-        #Check if user has made exclusion already
-        # if request.user.is_authenticated:
-        # ...
-        #     has_contacted = exclude_patch.objects.all().filter(patch_id=patch_id)
-        #     if has_contacted:
-        #         messages.error(request, 'You have already made an inquiry for this listing')
-                # return redirect('/approvallist')               
+
+        #Check if user has made inquiry already
+        if request.user.is_authenticated:
+            has_contacted = exclude_patch.objects.all().filter(patch_id=patch_id, client=client)
+            if has_contacted:
+                messages.error(request, 'You have already made an exception for this patch')
+                return redirect('dashboard')
                
         exclude = exclude_patch(patch_id=patch_id, client=client, title=title, justification=justification, exclude_date=exclude_date)
         
@@ -32,5 +26,3 @@ def exclude(request):
         messages.success(request, "Your request has been submitted, an approver will get back to you soon")
 
         return redirect('dashboard')
-
-        #The render function Combines a given template with a given context dictionary and returns an HttpResponse object with that rendered text.
