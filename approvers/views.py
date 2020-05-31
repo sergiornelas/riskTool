4888aaa6bdf0e11ab9ecb0bfbdc25609e3d4c091
sergiornelas <sergio.ornelas.92@outlook.com>
2020-05-30 23:34:59 -0500

    ya funciona la tabla de aprobadores

--- approvers/views.py
+++ approvers/views.py
@@ -20,6 +20,7 @@ from .models import authorize_Exception
 from django.http import HttpResponse
 from django import forms
 from django.forms import ModelForm
+from django.core import serializers
 #*
 
 def approvalsList(request):
@@ -48,108 +49,61 @@ def approvalsList(request):
         return render(request, 'pages/index.html')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def approvalDetail(request, exclude_patch_ID):
-    #get_object_or_404 will only return one object, get_list_or_404 multiple objects. Levanta la excepción Http404 si no existe el objeto.
-    #get_object_or_404 uses get(), get_list_or_404 uses filter()
-    #__in se utiliza cuando quieres usar de referencia objetos con otros objetos.
-
-#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     #FRONTEND SIMPLE QUERIES
-
         # exclude_patch_ID = 8
-    exception = get_object_or_404(exclude_patch, pk=exclude_patch_ID)
+
+    exceptionQuery = get_object_or_404(exclude_patch, pk=exclude_patch_ID)
         #Selecciona el objeto que contiene el {id 8 en exclude_patch model}:
             #<id=8, title=elID2, just=awef, patch_id=2>
-    patch_exc = get_object_or_404(patch, pk=exception.patch_id)
+    patch_exc = get_object_or_404(patch, pk=exceptionQuery.patch_id)
         #Selecciona el objeto que contiene en el id, dentro del patch model, el {valor de patch_id en el objeto exception}:
             #<id=2, server=Linux, crit=Medium, client_id=3>
 
-#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    #APPROVERS TABLE
-
-    #patch_approver = patchApproverRelationship.objects.filter(patch=exception.patch_id)
-        #Selecciona los objetos que contienen en el campo patch_id (patch) dentro del patchApproverRelationship model, el valor {patch_id en el objeto exception}:
-            #<id=1, approver_id=4, patch_id=2>,
-            #<id=4, approver_id=6, patch_id=2>,
-            #<id=8, approver_id=5, patch_id=2>,
-    #approver_detail = User.objects.filter(pk__in=patch_approver.values_list('approver_id'))
-    
-    approver_detail = User.objects.filter(pk__in=patchApproverRelationship.objects.filter(patch=exception.patch_id).values_list('approver_id'))
-            #<id=4, username=approver, is_active=1 ...>,
-            #<id=5, username=approver2, is_active=1 ...>
-            #<id=6, username=approver3, is_active=1 ...>
-    
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    #TESTING
-
-    #print("exclude_patch_ID = ", exclude_patch_ID, "\n")
-    #print("excepcion = " , exception, "\n")
-    #print("patch_exc = " , patch_exc, "\n")
     
-    print("")
-
-    approverResponsable = []
+    approver_detail = User.objects.filter(pk__in=patchApproverRelationship.objects.filter(patch=exceptionQuery.patch_id).values_list('approver_id'))
+                                                                                                                        #con values_list le damos a entender que queremos los valores de approver_id
+                                                                                                                        # y no los del id directo de pachapproverrelationship
     
-    for x in range(len(approver_detail)):
-        approverResponsable.append(approver_detail[x])
-        print("aprobadores responsables a esta excepcion: [",x,"] = " , approverResponsable[x])
+    authorize = authorize_Exception.objects.filter(exception_id=exceptionQuery.id).filter(approver_id__in =approver_detail)
 
+    approver_detail_pending = approver_detail.exclude(pk__in=authorize.values_list('approver_id'))
 
-    # existen autorizaciones ya realizadas en el modelo authorize_Exception
-    authorize = authorize_Exception.objects.filter(exception_id=exception.id).filter(approver_id__in=approver_detail)
-
-    print("")
-    exceptionReply = []
+    # TESTING
+    # print("exclude_patch_ID = ", exclude_patch_ID, "\n")
+    # print("excepcion = " , exception, "\n")
+    # print("patch_exc = " , patch_exc, "\n")
+    # print("approver_detail ",approver_detail)
+    # print("")
+    # print("authorize ",authorize)
+    # print("")
+    # print("approver_detail_pending ",approver_detail_pending)
     
-    for x in range(len(authorize)):
-        exceptionReply.append(authorize[x])
-        print("excepcion ya respondida: [",x,"] = " , exceptionReply[x])
-        #if exceptionReply[x]:
-    
-    
-    # for x in range(len(approver_detail)):
-    #     #if approverResponsable[x].objects.filter(pk__in=
-    #     if approverResponsable[x].objects.filter(id=1):
-    #         print("es 1")
+    # for i in approver_detail:
+    #     for j in authorize:
+    #         if i.id == j.approver_id:
+    #             print(i," y ",j," tienen el mismo id")
+    #         else:
+    #             print(i," y ",j," NO tienen el mismo id")
+      
 
+    # approveObjects = zip(authorize, approver_detail_pending)
     
-    if authorize:
-        print ("\nExisten campos en la tabla\n")
-    else:
-        print ("\nNO Existen campos en la tabla\n")
-
-#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    #CONTEXT
+    # if authorize:
+    #     print ("\nExisten autorizaciones de los aprobadores responsables en la excepcion dentro de la tabla\n")
+    # else:
+    #     print ("\nNo existen autorizaciones de los aprobadores responsables en la excepcion dentro de la tabla\n")
 
     context = {
-        'exception': exception,
+        'exceptionQuery': exceptionQuery,
         'patch_exc':patch_exc,
         'approver_detail':approver_detail,
-
-        #es resultado es el mismo:
         'authorize':authorize,
-        'exceptionReply':exceptionReply
+        'approver_detail_pending':approver_detail_pending,
     }
 
-#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    #URLS
-
     path = patchApproverRelationship.objects.filter(approver_id=request.user.id).filter(patch_id=patch_exc.id).values_list('approver_id', flat=True)
-        #var path = to user_authenticated_id
     
     if request.user.is_authenticated:
         #try:
@@ -164,18 +118,6 @@ def approvalDetail(request, exclude_patch_ID):
         return redirect('login')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 def authorize(request):
     if request.method == 'POST':
         exception_id = request.POST['exception_id']
@@ -186,3 +128,23 @@ def authorize(request):
     validate = authorize_Exception(exception_id=exception_id, approver=approver, state=state, comment=comment)
     validate.save()
     return redirect('approvalsList')
+
+
+
+#NOTAS:
+    #get_object_or_404 will only return one object, get_list_or_404 multiple objects. Levanta la excepción Http404 si no existe el objeto.
+        #get_object_or_404 uses get(), get_list_or_404 uses filter()
+        
+    #__in se utiliza cuando quieres usar de referencia objetos con otros objetos.
+
+    #.values(,)
+        #returns a QuerySet containing dictionaries
+            #<QuerySet [{'comment_id': 1}, {'comment_id': 2}]>
+
+    #.values_list(,)
+        #returns a QuerySet containing tuples
+            #<QuerySet [(1,), (2,)]>
+
+    #If you are using values_list() with a single field, you can use flat=True to return a QuerySet of single values instead of 1-tuples:
+    #flat=True will remove the tuples and return the list
+        #<QuerySet [1, 2]>
