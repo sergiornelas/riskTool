f4c0c092f2b500d22ddc7318e072cdadf19b6b49
sergiornelas <sergio.ornelas.92@outlook.com>
2020-06-04 03:07:25 -0500

    cuadro de dialogos y unas cuantas consultas con jQuery

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from patches.models import PATCHES
-from servers.models import SERVER_USER_RELATION
+from servers.models import SERVER_USER_RELATION, SERVER
 
 def dashboard(request):
     client_has_server=SERVER_USER_RELATION.objects.filter(user_id=request.user.id)
@@ -12,11 +12,16 @@ def dashboard(request):
         servers_ids.append(server.server_id)
 
     patches = PATCHES.objects.filter(server_id__in=servers_ids) #faltaría filtrar aqui con el status_id=2
+
+    serversPoll = SERVER.objects.filter(pk__in=client_has_server)
     
+    
+
     context = {
 		'client_has_server':client_has_server,
-        'patches':patches
-    }
+        'patches':patches,
+        'serversPoll':serversPoll
+    } 
 
     if request.user.is_authenticated:
         if request.user.profile.role == 1:
@@ -26,28 +31,3 @@ def dashboard(request):
             return redirect('index')
     else:
         return render(request, 'pages/index.html')
-
-
-
-
-
-
-
-
-
-
-# def dashboard(request):
-#     client_patches = patch.objects.filter(client=request.user.id)
-
-#     context = {
-# 		'patches': client_patches
-#     }
-
-#     if request.user.is_authenticated:
-#         if request.user.profile.role == 1:
-#             return render(request, 'clients/dashboard.html', context)
-#         else:
-#             messages.error(request, 'Not allowed to enter here')
-#             return redirect('index')
-#     else:
-#         return render(request, 'pages/index.html')
--- riskManagement/static/js/main.js
+++ riskManagement/static/js/main.js
@@ -4,3 +4,23 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function() {
     $('#message').fadeOut('slow');
 }, 3000);
+
+
+$("div[id^='myModal']").each(function(){
+  
+    var currentModal = $(this);
+    
+    //click next
+    currentModal.find('.btn-next').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
+    });
+    
+    //click prev
+    currentModal.find('.btn-prev').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
+    });
+  
+  });
+  
